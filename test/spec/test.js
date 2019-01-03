b1dd3e8ef990ce919947d783f735a8f3ff494bb5
Lillianna Blair <lblair@lyonscg.com>
2019-01-03 17:41:04 -0500

    inital commit testing that my js is in scope

---
+++ test/spec/test.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  describe('Pencil Durability code kata unit tests', function () {
+    it('should run here few assertions', function () {
+      expect(test).toEqual("this is working");
+    });
+  });
+})();
